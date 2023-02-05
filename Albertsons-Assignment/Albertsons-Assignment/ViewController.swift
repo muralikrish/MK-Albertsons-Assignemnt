5a6a477d942f5f6484d505c246d546ae479f5338
MuraliKrishna <krishnaveeramosu@gmail.com>
2023-02-05 14:40:52 -0500

    [MK-Albertsons-Assignemnt] Designed the view, Added a stack view with Image, label.  Added gesture recognizer.

--- Albertsons-Assignment/Albertsons-Assignment/ViewController.swift
+++ Albertsons-Assignment/Albertsons-Assignment/ViewController.swift
@@ -10,12 +10,24 @@ import Alamofire
 
 class ViewController: UIViewController, Storyboarded {
     weak var coordinator: MainCoordinator?
-
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var fact: UILabel!
+    @IBOutlet var tapGesture: UITapGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.setUp()
     }
-
+    
+    func setUp() {
+        self.tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
+    @IBAction func viewTapped(_ sender: UITapGestureRecognizer) {
+        print("did tap view", sender)
+    }
 
 }
 
