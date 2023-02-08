c9cb3dc2e32c8cbadff796dc6c5e95c48084cb41
MuraliKrishna <krishnaveeramosu@gmail.com>
2023-02-07 19:34:36 -0500

    [MK-Albertsons-Assignemnt] Removed default UI Tests

--- Albertsons-Assignment/Albertsons-AssignmentUITests/Albertsons_AssignmentUITests.swift
+++ Albertsons-Assignment/Albertsons-AssignmentUITests/Albertsons_AssignmentUITests.swift
@@ -21,15 +21,6 @@ class Albertsons_AssignmentUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
     
     func testThatUIElementsPresent() {
         let app = XCUIApplication()
@@ -38,15 +29,6 @@ class Albertsons_AssignmentUITests: XCTestCase {
         XCTAssert(image.exists) // checking imageView present.
         let label = app.staticTexts["ViewController.fact"]
         XCTAssert(label.exists) //chekcing if label present
-        
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
